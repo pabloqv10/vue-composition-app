b08e8580f385097a9928d2d58ceedd559bfda100
Pablo Quiceno <pabloquiceno95@gmail.com>
2024-07-06 11:38:17 -0500

    Initial commit

---
+++ src/counter/views/CounterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+<template>
+  <h1>Counter View: {{ counter }}</h1>
+  <button @click="counter++">+1</button>
+</template>
