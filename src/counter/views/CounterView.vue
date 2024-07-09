0deb1dfc4d52b143d0d52b1c74e336e0e98866f7
Pablo Quiceno <pabloquiceno95@gmail.com>
2024-07-08 19:54:33 -0500

    Pokemon views and layouts was added into project

--- src/counter/views/CounterView.vue
+++ src/counter/views/CounterView.vue
@@ -1,9 +1,10 @@
+<template>
+  <h1>Counter View: {{ counter }}</h1>
+  <button @click="counter++">+1</button>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 
 const counter = ref(0)
 </script>
-<template>
-  <h1>Counter View: {{ counter }}</h1>
-  <button @click="counter++">+1</button>
-</template>
---
+++ src/pokemons/layouts/PokemonLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Pokemon layout</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
